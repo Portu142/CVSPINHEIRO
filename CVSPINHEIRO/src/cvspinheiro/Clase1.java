cf43f62687e958cd5c0e08b46e9cc3c6fb718360
dam <dam@203P07>
2023-02-17 09:49:13 +0100

    Primer commit

---
+++ CVSPINHEIRO/src/cvspinheiro/Clase1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cvspinheiro;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ricardo Daniel Pinheiro Carvalho");
+        System.out.println("17/02/2023");
+    }
+    
+}
